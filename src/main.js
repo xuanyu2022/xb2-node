f027e82b476e5651fcfc4c1651b3f6f1477ae155
lilei <xuanyu200910@163.com>
2022-03-25 11:54:56 +0800

    创建web服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-const greeting = '你好啊_greeting_';
-console.log(greeting);
+// const greeting = '你好啊_greeting_';
+// console.log(greeting);
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('hello~');
+  response.end();
+}
+);
+
+server.listen(3000, () => {
+  console.log('火箭服务已启动');
+})
---
+++ src/playground/index.js
@@ -0,0 +1,31 @@
+// console.log('你好!');
+// console.log('爱你奥!');
+
+// function log(message, prefix) {
+//   console.log(prefix, message);
+// };
+// log('hello~', 'prefix的参数');
+// log('你好', '日志');
+
+
+
+// const log = () => {
+
+//   console.log('LOG::');
+// };
+
+// log();
+
+// const greet = (name) => {
+//   return '你好 ' + '-' + name;
+// }
+
+// // const greet = name => return '你好' + name;
+
+
+// const greeting = greet('李磊');
+
+// console.log(greeting);
+
+
+
