c1af843c0fb89356b507a3861ee635f0a3afdccc
lilei <xuanyu200910@163.com>
2022-03-26 16:15:58 +0800

    客户端请求,服务端响应

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 port = 3000;
+
+/**
+ *使用JSON中间件
+ **/
+
+app.use(express.json());
+
+
 
 app.listen(port, () => {
   console.log(' 服务器已启动');
@@ -23,7 +32,7 @@ const data = [
     content: '会当凌绝顶,一览众山小',
   },
   {
-    id: 4,
+    id: 3,
     title: '忆江南',
     content: '日出江花红似火, 春来江水绿如蓝',
   },
@@ -38,4 +47,28 @@ app.get('/posts/:postId', (request, response) => {
   const { postId } = request.params;
   const posts = data.filter(item => item.id == postId);
   response.send(posts[0]);
+});
+//设置响应的头部数据
+response.set('Sing-Along', 'How I wonder what you are');
+/**
+ * 创建内容
+ */
+
+app.post('/posts', (request, response) => {
+  const { content } = request.body;
+  /**
+   * 设置响应状态码
+   */
+  response.status(201);
+
+  // 输入'请求的header数据
+  console.log(request.headers['sing-along']);
+
+
+
+
+  //做出响应
+  response.send({
+    message: `成功创建了内容:${content}`
+  });
 });
