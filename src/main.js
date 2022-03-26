040a7c434c73e86221a4165d0c34216cf346797c
lilei <xuanyu200910@163.com>
2022-03-26 10:40:34 +0800

    创安装应用框架, 创建web服务器<expers.js>,定义响应JSON的应用接口, 定义带参数的应用接口

--- src/main.js
+++ src/main.js
@@ -1,14 +1,41 @@
-// const greeting = '你好啊_greeting_';
-// console.log(greeting);
+const express = require('express');
+const app = express();
+port = 3000;
 
-const http = require('http');
+app.listen(port, () => {
+  console.log(' 服务器已启动');
+});
 
-const server = http.createServer((request, response) => {
-  response.write('hello~');
-  response.end();
-}
-);
+app.get('/', (req, res) => {
+  res.send('你好啊');
 
-server.listen(3000, () => {
-  console.log('火箭服务已启动');
-})
+});
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山, 苍茫云海间',
+  },
+  {
+    id: 2,
+    title: '望岳关山月',
+    content: '会当凌绝顶,一览众山小',
+  },
+  {
+    id: 4,
+    title: '忆江南',
+    content: '日出江花红似火, 春来江水绿如蓝',
+  },
+];
+
+
+app.get('/posts', (request, response) => {
+  response.send(data);
+});
+
+app.get('/posts/:postId', (request, response) => {
+  const { postId } = request.params;
+  const posts = data.filter(item => item.id == postId);
+  response.send(posts[0]);
+});
