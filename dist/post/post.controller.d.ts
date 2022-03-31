591de5e014c0ac6d96dc968b1cac28eba66663a4
lilei <xuanyu200910@163.com>
2022-03-31 15:39:12 +0800

    安装安装驱动连接服务器

--- dist/post/post.controller.d.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const index: (request: Request<import("express-serve-static-core").ParamsDictionary>, response: Response<any>, next: NextFunction) => void;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,4 +7,15 @@ dotenv.config();
  */
 
 //用解构的方法, 从process.env 导出APP_PORT的值
-export const { APP_PORT } = process.env;
+export const { 
+  APP_PORT 
+} = process.env;
+
+//数据仓库配置
+export const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_PASSWORD,
+  MYSQL_USER,
+  MYSQL_DATABASE,
+} = process.env;
