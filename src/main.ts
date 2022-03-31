c6d06143f1b6d4ce86e8c4a64cbdb5dcbb846ffd
lilei <xuanyu200910@163.com>
2022-03-31 16:09:12 +0800

    测试连接数据仓库

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import app from './app';
 import { APP_PORT } from './app/app.config';
+import {connection} from './app/database/mysql'
 
 app.listen(APP_PORT, () => {
   console.log('服务已启动');
 });
 
+
+//测试使用数据服务连接
+
+connection.connect(error => {
+  if(error){
+    console.log('连接服务器失败:',error.message);
+    return;
+  }
+  console.log('成功连接数据服务 ~~');
+});
