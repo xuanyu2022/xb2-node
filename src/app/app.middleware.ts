35aa68611a7dcf22cf3d69273235b4469ff5c4e0
lilei <xuanyu200910@163.com>
2022-04-01 19:11:18 +0800

    处理创建用户的错误_修改默认的异常处理器

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -31,11 +31,23 @@ export const defaultErrorHandler = (
   let statusCode: number, message: string;
   //处理异常
   switch (error.message) {
+    case 'NAME_IS_REQUIRED':
+      statusCode = 400;
+      message = '请提供用户名';
+      break;
+    case 'PASSWORD_IS_REQUIRED':
+      statusCode = 400;
+      message = '请提供用户密码';
+      break;    
+    
     default:
       statusCode = 500;
       message = '服务暂时出了点问题 ~~';
       break;
   }
+
+
+
 
   response.status(statusCode).send({ message });
 };
