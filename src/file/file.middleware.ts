568e2931c2a259e1004ec4ce4628a5b906ad6d96
lilei <xuanyu200910@163.com>
2022-04-06 18:24:37 +0800

    定义上传文件接口

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -8,3 +8,8 @@ import multer from 'multer';
 const fileUpload = multer ({
   dest:'upload/',
 });
+
+/** 文件拦截器 */
+export const fileInterceptor = fileUpload.single('file');
+
+
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import * as fileController from './file.controller';
-
+import {authGuard } from '../auth/auth.middleware';
+import {fileInterceptor} from './file.middleware';
 const router = express.Router();
 
-
-
+router.post ('file',authGuard,fileInterceptor,fileController.store);
 export default router;
 
