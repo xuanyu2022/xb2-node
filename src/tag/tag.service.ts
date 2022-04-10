86cf7e6ca2b19a7eea5534a80199eab0e93971ae
lilei <xuanyu200910@163.com>
2022-04-10 18:59:59 +0800

    按名字查找标签

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,2 +1,31 @@
 import { connection } from '../app/database/mysql';//处理器方法接受的参数的类型
+import {TagModel} from './tag.model';
 
+
+/** 定义存储标签数据的功能*/
+export const NAME= async ( 
+  tag:TagModel    
+) =>{
+    const statement = `
+        INSERT INTO tag
+        SET ?
+    `;
+
+    const [data] = await connection.promise().query(statement,tag);
+    return data as any;
+
+};
+
+/**
+ * 按名字查找标签
+ */
+
+export const getTabByName = async (tagName:string) => {
+
+        const statement = `
+            SELECT id,name FROM tag
+            WHERE name=?
+        `;
+        const [data] = await connection.promise().query(statement,tagName);
+        return data[0];
+}
