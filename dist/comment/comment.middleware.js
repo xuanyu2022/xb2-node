c6f2942b5918764839bacd76b20c1a2e71d804dc
lilei <xuanyu200910@163.com>
2022-04-24 15:40:53 +0800

    获取内容的评论列表

--- dist/comment/comment.middleware.js
+++ dist/comment/comment.middleware.js
@@ -1,10 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.filter = async (request, response, next) => {
+    const { post, user, action } = request.query;
     request.filter = {
         name: 'dafault',
         sql: 'comment.parentId IS NULL'
     };
+    if (post && !user && !action) {
+        request.filter = {
+            name: 'postComments',
+            sql: 'comment.parentId IS NULL AND comment.postId = ?',
+            param: `${post}`,
+        };
+    }
     next();
 };
 //# sourceMappingURL=comment.middleware.js.map
--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -9,9 +9,26 @@ export const filter= async (
   response:Response,
   next:NextFunction,
 ) =>{
+        const {post,user,action} = request.query;
+  
         request.filter = {
           name:'dafault',
           sql:'comment.parentId IS NULL'
         }
+
+        if(post && !user && !action ){
+
+            request.filter ={
+              name:'postComments',
+              sql:'comment.parentId IS NULL AND comment.postId = ?',
+              param:`${post}`,
+            }
+        }
+
+        
+
+
+
+
       next();
 };
