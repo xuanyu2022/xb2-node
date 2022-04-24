e561d80fb96175523e8b827e0af0b66e50b849fe
lilei <xuanyu200910@163.com>
2022-04-24 11:19:30 +0800

    查询片段

---
+++ dist/comment/comment.provider.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sqlFragment = {
+    leftJoinUser: `
+    LEFT JOIN user
+      ON user.id = comment.userId
+    LEFT JOIN avatar
+      ON avatar.userId = user.id
+  `,
+    user: `
+    JSON_OBJECT(
+      'id',user.id,
+      'name',user.name,
+      'avatar',IF(COUNT(avatar.id),1,NULL)
+    ) as user
+  `,
+    leftJoinPost: `
+      LEFT JOIN post
+        ON post.id= comment.postId
+
+  `,
+    post: `
+      JSON_OBJECT(
+        'id', post.id,
+        'title',post.title,
+      ) as post
+  `,
+};
+//# sourceMappingURL=comment.provider.js.map
